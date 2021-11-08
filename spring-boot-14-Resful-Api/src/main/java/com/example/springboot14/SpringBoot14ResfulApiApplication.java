db5b11da429d7f1a3fa1be04ef6c519c97ff3ad0
MD3158 <phamminh3198@gmail.com>
2021-11-08 13:47:26 +0700

    RestApi - fail

---
+++ spring-boot-14-Resful-Api/src/main/java/com/example/springboot14/SpringBoot14ResfulApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot14ResfulApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot14ResfulApiApplication.class, args);
+    }
+
+}
---
+++ spring-boot-14-Resful-Api/src/main/java/com/example/springboot14/Todo.java
@@ -0,0 +1,8 @@
+package com.example.springboot14;
+import lombok.Data;
+
+@Data
+public class Todo {
+    private String title;
+    private String detail;
+}
